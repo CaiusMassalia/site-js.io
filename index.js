c5734db58c93da089e4db098c3cb131b5c57db29
CaiusMassalia <72454928+CaiusMassalia@users.noreply.github.com>
2020-11-27 14:23:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,131 @@
+ScrollReveal().reveal('.lolilol',{ delay: 2500 });
+
+
+
+
+//const scientist = {
+  //  firstName: "Albert",
+    //lastName: "Einstein",
+    //job: "physicien"
+//}
+
+//scientist.firstName='robert';
+//console.log(scientist.firstName);
+
+//const villes = [];
+//villes.push('paris');
+//console.log(villes.pish('paris'));
+
+
+
+//let word = 'simplon';
+//let firstLetter = word[0].toUpperCase();
+//let restOfTHeWord = word.substr(1).toLowerCase();
+//let fullWord = firstLetter + restOfTHeWord;
+//console.log(fullWord);
+
+//word = 'avangers';
+//firstLetter = word[0].toUpperCase();
+//restOfTHeWord = word.substr(1).toLowerCase();
+//fullWord = firstLetter + restOfTHeWord;
+//console.log(fullWord);
+
+//word = 'voiture';
+//firstLetter = word[0].toUpperCase();
+//restOfTHeWord = word.substr(1).toLowerCase();
+//fullWord = firstLetter + restOfTHeWord;
+//console.log(fullWord);
+
+//const capitalize = (word) => {
+  //  let firstLetter = word[0].toUpperCase();
+    //let restOfTHeWord = word.substr(1).toLowerCase();
+//let fullWord = firstLetter + restOfTHeWord;
+//return fullWord;
+//}
+
+//capitalize('simplon');
+
+
+//[CODE] const tab = [3, 5, 7, 4, 8];
+//1 - 
+//2 - 
+// [CODE]for(let index = 0; index < tab.length; index++){
+    //[CODE]console.log(tab[index] * tab[index]);}
+
+//TODO: 
+//1 - Mettre la première lettre de ('Marseille')
+// [CODE]const ville = 'marSeillE';
+// [CODE]console.log(ville[0]);
+
+//const ville = 'paris';
+//Je trouve comment isoler la première lettre de 'ville' ==> google !!!
+//Je déclare une variable dans laquelle j'affecte la première lettre 
+//const firstLetter = ville[0];
+//Je l'affiche ! 
+//console.log(firstLetter);
+
+
+
+// Let = je permet à ma vairable de varier. / Const = je ne permet pas à ma variable de varier.
+// UNE VARIABLE NE SE DÉCLARE QU'UNE SEULE FOIS !!!
+
+
+
+//const password = "Jedeviles88"
+// console.log(password.length);
+
+//const str = 'Life, the universe and everything. Answer:';
+//console.log(`${str} ${str.length}`);
+
+//const password = "simplon13"
+//console.log(password.length)
+
+//const lastLetter = password.charAt(5);
+//const lastletter = password[5];
+//console.log(lastLetter);
+
+//const firstName = "John";
+//const lastName ="Doe";
+//const fullName =`Mon nom complet est ${firstName} ${lastName}`; 
+//const fullName2 = "Mon nom complet est" + firstName + '' +lastName;
+
+//const ville = "Athena";
+//const ville2= "Massalia";
+//const ville3= "Roma";
+//const ville4= "Sparta"
+
+//const villeEnMaj= ville.toUpperCase();
+//const villeEnMaj2= ville2.toUpperCase();
+//const villeEnMaj3= ville3.toUpperCase();
+//const villeEnMaj4= ville4.toUpperCase();
+
+//const villes = ["Athena", "Massalia", "Roma", "Sparta"];
+//const firstEl = villes[0].toUpperCase();
+//const secondEl = villes[1].toUpperCase();
+
+//villes.forEach((ville)=>{
+//    console.log(ville.toUpperCase()); });
+
+
+//const age =15; 
+//if(age < 18){
+//    console.log('je ne peux pas voter')
+//}else{
+//    console.log('je peux voter')
+//} 
+
+
+//Déclarer deux variables avec comme valeur 4 et 5
+//Créez une autre variable contenant l'addition des deux et l'afficher.
+//TODO
+//var a = 5;
+//var b = 4;
+//var C = a + b; 
+//console.log (a + b);
+
+//Afficher la chaine de caractère suivante en majuscule
+//let team = 'avengers';
+//console.log(team.toUpperCase())
+
+//afficher la chaine de caractère suivante excepté la première lettre.
+//let team = 'avengers';
